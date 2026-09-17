ea0a94544bbddd8580dddaa77920147df1fcfe05
taylorfuncheon <taylorfuncheon@gmail.com>
2026-09-17 19:45:15 -0400

    Enhance evaluate function for better expression handling
    Updated evaluate function to handle various expressions and edge cases, including single numbers and unknown operators.

--- evaluate.scm
+++ evaluate.scm
@@ -31,3 +31,34 @@ expr
 20
 ; Evaluate function is 7 lines long
 ; I used way B with assq to match each operator to the function it should use. The evaluate function checks if the expression is just a number and if it's not, it finds the operator, solves the numbers inside and then does the final calculation.
+> (evaluate '(+ 1 2))
+3
+> (evaluate '(* (+ 2 3) 4))
+20
+> (evaluate '(- (* 6 7) (/ 10 2)))
+37
+> (evaluate '(+ 1 (* 2 (- 10 (/ 8 4)))))
+17
+> (evaluate 42)
+42
+> (evaluate '(+ 1))
+1
+; I chose (evaluate '(+ 1)) since it only has one number. I wanted to see if it would still work with only one operator.
+
+;unknown operator
+> (evaluate '(+ 1 (& 2 3)))
+*** ERROR IN evaluate, console@19:11 -- unknown operator: &
+
+; Option A
+2> (evaluate '(+ 1 2 3 4))
+10
+2> (evaluate '(* 2 3 4 5))
+120
+3> (evaluate '(+ 5))
+5
+3> (evaluate '(+))
+0
+3> (evaluate '(- 5))
+-5
+; I chose A for using any number of arguments. My evaluator already worked because map goes through every value and apply sends them to the operator. 
+
